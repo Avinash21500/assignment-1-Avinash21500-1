87214d49ada5b0b97f5dcb21b4f16851599bb2eb
Avinash Dubey <avinash.dubey_da18@gla.ac.in>
2020-02-23 13:39:02 +0530

    Added delete functionaliy

--- src/MyMain/MyMain.java
+++ src/MyMain/MyMain.java
@@ -10,7 +10,7 @@ public class MyMain {
     public static void main(String[] args) {
         MyList obj=new MyList();
         Scanner sc =new Scanner(System.in);
-        System.out.println("Welcome to DBC's Contact LIst App");
+        System.out.println("Welcome to Avinash's Contact LIst App");
         System.out.println("Press 1 to add a new contact");
         System.out.println("Press 2 to view all contacts");
         System.out.println("Press 3 to search for a contact");
@@ -64,13 +64,13 @@ public class MyMain {
                         System.out.println("0 match found");
                     }
                     break;
-                /*case 4:
+                case 4:
                     System.out.println("Here are all your contacts:");
                     obj.printList();
                     System.out.print("Please enter first name whose contact you want delete:- ");
                     String delfname=sc.nextLine();
                     obj.delete(delfname);
-                    break;*/
+
             }
 
             System.out.println("Press 1 to add a new contact");
--- src/mList/MyList.java
+++ src/mList/MyList.java
@@ -30,83 +30,120 @@ public class MyList {
         System.out.println("---Here are all your contacts---");
         System.out.println("-------- * -------- * -------- * --------");
         Node temp=head;
-        while(temp!=null){
-            Person e=temp.getData();
-            System.out.println("First Name: "+e.getFname());
-            System.out.println("Last Name: "+e.getLname());
-            System.out.print("Contact Number(s): ");
-            int[] no=e.getConNumber();
-            for (int i = 0; i <no.length; i++) {
-                if(no[i]!=0) {
-                    System.out.print(no[i]);
-                    if(no[i+1]!=0){
-                        System.out.print(", ");
-                    }
-                }
-            }
-            System.out.println();
-            if(e.getEmail()!=null) {
-                System.out.println("Email address: " + e.getEmail());
-            }
-            System.out.println("-------- * -------- * -------- * --------");
-            temp=temp.getNext();
-
-        }
-    }
-
-    public boolean search(String fname) {
-        boolean response = false;
-        //traversing the linked list and will check each node for given data
-        // assigning head to temp
-        Node temp = head;
-        while (temp != null) {
-            // checking for data at each node
-            Person temp1=temp.getData();
-            if (temp1.getFname().equals(fname)){
-                response = true;
-                System.out.println("1 match found!");
-                System.out.println("-------- * -------- * -------- * --------");
-                System.out.println("First Name: "+temp1.getFname());
-                System.out.println("Last Name: "+temp1.getLname());
+        if(head!=null) {
+            while (temp != null) {
+                Person e = temp.getData();
+                System.out.println("First Name: " + e.getFname());
+                System.out.println("Last Name: " + e.getLname());
                 System.out.print("Contact Number(s): ");
-                int[] no=temp1.getConNumber();
-                for (int i = 0; i <no.length; i++) {
-                    if(no[i]!=0) {
+                int[] no = e.getConNumber();
+                for (int i = 0; i < no.length; i++) {
+                    if (no[i] != 0) {
                         System.out.print(no[i]);
-                        if(no[i+1]!=0){
+                        if (no[i + 1] != 0) {
                             System.out.print(", ");
                         }
                     }
                 }
                 System.out.println();
-                if(temp1.getEmail()!=null) {
-                    System.out.println("Email address: " + temp1.getEmail());
+                if (e.getEmail() != null) {
+                    System.out.println("Email address: " + e.getEmail());
                 }
-                temp=temp.getNext();
                 System.out.println("-------- * -------- * -------- * --------");
-                response=true;
-                break;
+                temp = temp.getNext();
+
             }
-            // updating temp
-            temp = temp.getNext();
+        }
+        else{
+            System.out.println("||        Contact List is Empty         ||");
+            System.out.println("-------- * -------- * -------- * --------");
+        }
+    }
+
+    public boolean search(String fname) {
+        boolean response = false;
+        Node temp = head;
+        if(head!=null) {
+            while (temp != null) {
+                Person temp1 = temp.getData();
+                if (temp1.getFname().equals(fname)) {
+                    response = true;
+                    System.out.println("1 match found!");
+                    System.out.println("-------- * -------- * -------- * --------");
+                    System.out.println("First Name: " + temp1.getFname());
+                    System.out.println("Last Name: " + temp1.getLname());
+                    System.out.print("Contact Number(s): ");
+                    int[] no = temp1.getConNumber();
+                    for (int i = 0; i < no.length; i++) {
+                        if (no[i] != 0) {
+                            System.out.print(no[i]);
+                            if (no[i + 1] != 0) {
+                                System.out.print(", ");
+                            }
+                        }
+                    }
+                    System.out.println();
+                    if (temp1.getEmail() != null) {
+                        System.out.println("Email address: " + temp1.getEmail());
+                    }
+                    temp = temp.getNext();
+                    System.out.println("-------- * -------- * -------- * --------");
+                    response = true;
+                    break;
+                }
+                // updating temp
+                temp = temp.getNext();
+
+
+            }
 
         }
+        else{
+            System.out.println("-------- * -------- * -------- * --------");
+            System.out.println("||        Contact List is Empty         ||");
+            System.out.println("-------- * -------- * -------- * --------");
+        }
+
         return response;
     }
 
-    /*public void delete(String delfname){
-        Node temp=head;
-        while(temp!=null){
-            Node tempy=temp.getNext();
-            Person temp1=tempy.getData();
-            if(temp1.getFname().equals(delfname)){
-                tempy=tempy.getNext();
-                temp.setNext(tempy);
-                System.out.println(delfname+"'s contact deleted from list!");
+    public void delete(String delfname) {
+        Node temp = head;
+        if (head != null) {
+            if(head.getData().getFname().equals(delfname)){
+                head=head.getNext();
+                temp=head;
+                System.out.println(delfname + "'s contact deleted from list!");
             }
-            temp=temp.getNext();
-        }
+            if (head.getNext() != null){
+                while (temp != null) {
 
-    }*/
+                    Person temp1 = temp.getData();
+                    if (temp.getNext().getData().getFname().equals(delfname) && temp.getNext()!=null) {
+                        Node temp7 = temp.getNext().getNext();
+                        temp.setNext(temp7);
+                        System.out.println(delfname + "'s contact deleted from list!");
+                        break;
+                    }
+                    else if(temp.getNext().getData().getFname().equals(delfname)&& temp.getNext().getNext()==null){
+                        temp.setNext(null);
+                    }
+                    temp = temp.getNext();
+                }
+            } else {
+                Person tempu = head.getData();
+                if (tempu.getFname().equals(delfname)) {
+                    head = null;
+                    System.out.println(delfname + "'s contact deleted from list!");
+                }
+            }
+
+        }
+        else{
+            System.out.println("-------- * -------- * -------- * --------");
+            System.out.println("||        Contact List is Empty         ||");
+            System.out.println("-------- * -------- * -------- * --------");
+        }
+    }
 
 }
